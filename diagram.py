f0f07dba323c381470dbeea2a67d2aca1d0b01d7
viks1102 <viks11021@gmail.com>
2025-07-01 13:30:54 +0100

    wip

--- diagram.py
+++ diagram.py
@@ -1,4 +1,4 @@
-from diagrams import Diagram
+from diagrams import Diagram, Node
 from diagrams.gcp.compute import ComputeEngine
 from diagrams.gcp.network import VPC
 from diagrams.gcp.database import SQL
@@ -13,6 +13,8 @@ def generate_diagram(components, filename="output/architecture"):
                 nodes[c['name']] = SQL(c['name'])
             elif c['type'] == 'vpc':
                 nodes[c['name']] = VPC(c['name'])
+            elif c['type'] == 'sa':
+                nodes[c['name']] = Node(c['name'])    
 
         for i in range(len(components) - 1):
             nodes[components[i]['name']] >> nodes[components[i + 1]['name']]
--- generator.py
+++ generator.py
@@ -11,28 +11,3 @@ def generate_component(component, output_dir):
     output_path = os.path.join(output_dir, f"{component['type']}.tf")
     with open(output_path, 'w') as f:
         f.write(rendered)
-
-
-### architecture-as-code/diagram.py
-
-from diagrams import Diagram
-from diagrams.gcp.compute import ComputeEngine
-from diagrams.gcp.network import VPC
-from diagrams.gcp.database import SQL
-from diagrams.gcp.security import Iam
-
-def generate_diagram(components, filename="output/architecture"):
-    with Diagram("GCP Architecture", outfilename=filename, show=False):
-        nodes = {}
-        for c in components:
-            if c['type'] == 'compute_instance':
-                nodes[c['name']] = ComputeEngine(c['name'])
-            elif c['type'] == 'cloudsql':
-                nodes[c['name']] = SQL(c['name'])
-            elif c['type'] == 'vpc':
-                nodes[c['name']] = VPC(c['name'])
-            elif c['type'] == 'sa':
-                nodes[c['name']] = Iam(c['name'])    
-
-        for i in range(len(components) - 1):
-            nodes[components[i]['name']] >> nodes[components[i + 1]['name']]
